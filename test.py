0ca33c52b090feeee7453ed7c244a6600171842d
predaalex <preda.alex30@yahoo.com>
2024-12-16 19:51:01 +0200

    update

---
+++ test.py
@@ -0,0 +1,17 @@
+import pydirectinput
+import time
+
+# time.sleep(1)
+# presses = 10000
+# for i in range(presses):
+#     pydirectinput.keyDown("alt")
+#     pydirectinput.rightClick()
+#
+#     print(f"Pressed for {i}/{presses}")
+#     time.sleep(0.05)
+
+
+time.sleep(1)
+while True:
+    pydirectinput.press("2")
+    time.sleep(1)
